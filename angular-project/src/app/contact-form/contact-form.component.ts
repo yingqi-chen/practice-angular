31d1106d22642af7a416d9e01c48d13741bc8bf1
Yingqi <chanwk18@hotmail.com>
2020-07-06 16:55:31 -0600

    create a Formgroup object in Contact form component

--- angular-project/src/app/contact-form/contact-form.component.ts
+++ angular-project/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms'
 
 @Component({
   selector: 'app-contact-form',
@@ -11,5 +12,11 @@ export class ContactFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  form = new FormGroup({
+    name: new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl()
+  })
 
 }
